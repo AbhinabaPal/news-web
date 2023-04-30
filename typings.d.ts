9402c46ca8f35b0eeb64263dbd61632650ed0b36
Abhinaba <abpro6858@gmail.com>
2023-04-30 23:13:10 +0530

    feat(added-graphql):added graphql to existing project for news fetch

--- typings.d.ts
+++ typings.d.ts
@@ -6,3 +6,28 @@ type Category =
     | "technology"
     | "health"
     | "science";
+
+type NewsResponse = {
+    pagination: Pagination;
+    data: Article[];
+};
+
+type Pagination = {
+    count: Int;
+    limit: Int;
+    offset: Int;
+    total: Int;
+};
+
+type Article = {
+    author: string | null;
+    category: string;
+    country: string;
+    description: string;
+    image: string | null;
+    language: string;
+    publishedAt: string;
+    source: string;
+    title: string;
+    url: string;
+};
